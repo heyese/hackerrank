38805b4b90c76c7b5b9cd4f63bedc6f75405b4c7
Edmund <e_heyes@hotmail.com>
2019-03-06 14:48:36 +0000

    Waiter.py added

---
+++ Waiter.py
@@ -0,0 +1,61 @@
+
+import collections
+import math
+
+def primes():
+
+    num = 2
+    yield num
+    primes = [num]
+    while True:
+        num += 1
+        num_prime = True
+        for p in primes:
+            if p > math.sqrt(num):
+                break
+            if not num % p:
+                num_prime = False
+        if num_prime:
+            primes.append(num)
+            yield num
+
+def waiter(numbers, q):
+    prime_gen = primes()
+    plate_stacks = collections.defaultdict(list)
+    current_plates = collections.deque(numbers)
+    next_plates = collections.deque()
+
+    for i in range(q):
+        p = next(prime_gen)
+        next_plates.clear()
+        while True:
+            try:
+                plate = current_plates.pop()
+            except IndexError:
+                break
+
+            if plate % p:
+                next_plates.append(plate)
+            else:
+                plate_stacks[i].append(plate)
+
+        current_plates = next_plates.copy()
+
+    results = []
+    for i in range(q):
+        results += reversed(plate_stacks[i])
+    results += reversed(next_plates)
+    return results
+
+
+
+
+if __name__ == '__main__':
+
+    nq = input().split()
+    n = int(nq[0])
+    q = int(nq[1])
+    number = list(map(int, input().rstrip().split()))
+    result = waiter(number, q)
+
+    print('\n'.join(map(str, result)))
